3981f79bc1fc97f8742617de9fa27727d2a1b16f
ParkJiSu28 <qkrwltn28@gmail.com>
2019-05-19 22:26:05 +0900

    mocktest_setup

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import QuizViewSet, QuizRequestViewSet,subject_view
+from .views import QuizViewSet, QuizRequestViewSet,subject_view,MockTestViewSet
 
 router = DefaultRouter()
 router.register('problem', QuizViewSet)
 request_list = QuizRequestViewSet.as_view({
+    'get': 'list',
+})
+mocktest_list = MockTestViewSet.as_view({
     'get': 'list',
 })
 
@@ -13,4 +16,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('request', request_list),
     path('subject', subject_view),
+    path('mocktest',mocktest_list),
 ]
