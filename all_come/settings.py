882c75b0b3993a35971e20958d4e1c5828440496
ParkJiSu28 <qkrwltn28@gmail.com>
2019-05-25 19:43:09 +0900

    heroku

--- all_come/settings.py
+++ all_come/settings.py
@@ -118,10 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 
 db_from_env = dj_database_url.config(conn_max_age=500)
